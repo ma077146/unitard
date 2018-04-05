7a33cf00ce6867e12b623ffbdf2535944fb469cd
Mark Pruitt <mark.pruitt@peoplestrategy.com>
2018-04-05 13:50:03 -0400

    create template files for Three Wire Systems (TWS)

---
+++ client-forms/TWS/2-client-forms.sql
@@ -0,0 +1,54 @@
+-- Release Notes --
+-- Make sure to update the client.conf file to add this line -> define( 'CLIENT_CODEREF', 'TWS');
+-- Copy over files to clients directory - ps-client-forms.css and ps-client-forms.js
+-- Copy up PDF forms in the clients directory /pdf_files
+
+-- need to add DB name 
+-- for qa/demqa - implmnt_tws
+-- for production - erctaz_tws
+
+REPLACE INTO .client_forms
+(client_forms_id, class_name, is_active) VALUES
+(2, 'TermsAndConditions', 1);
+
+
+REPLACE INTO .client_forms_pdf
+(client_forms_id, language_id, pdf) VALUES
+(2, 1, 'Terms_and_Conditions.pdf');
+
+-- SELECT * FROM .form_fields;
+REPLACE INTO .form_fields
+(form_fields_id, field_type, html_name, html_field_attributes, html_wrapper_attributes, field_type_settings, options_group_id, default_value, is_encrypted) VALUES
+(200, 'Text', 'employee_name', NULL, NULL, NULL, NULL, '$jobseeker->getFormattedName()', 0);
+
+-- SELECT * FROM .form_fields_options;
+-- REPLACE INTO .form_fields_options
+-- (form_fields_options_id, options_group_id, option_value, display_order, is_active) VALUES
+-- These values are likely already used; check before using them!
+-- (1, 3, 'yes', 1, 1),
+-- (2, 3, 'no', 2, 1),
+-- (3, 2, '2', 2, 1),
+-- (4, 2, '3', 3, 1),
+-- (5, 2, '1', 1, 1);
+
+-- SELECT * FROM .form_steps;
+REPLACE INTO .form_steps
+(form_steps_id, form_reference_id, form_reference_code, step, actor) VALUES
+(200, 2, 'client_form', 1, 'job_seeker'),
+(201, 2, 'client_form', 2, 'admin');
+
+-- SELECT * FROM .form_steps_contents;
+REPLACE INTO .form_steps_contents
+(form_steps_contents_id, form_steps_id, form_fields_id, mlcode_html_block_label, display_order, parent, is_required, is_active, default_value, visibility) VALUES
+(1, 1, NULL, 'form_text', 2, 0, 0, 1, NULL, 1);
+
+REPLACE INTO .form_steps_contents_coordinates
+(form_reference_id, form_reference_code, form_steps_contents_id, data_key, entry_number, language_id, pdf_page, x, y, x2, y2, pdflib_options) VALUES
+(2, 'client_form', 69, 0, 1, 1, 1, 450, 610, 0, 0, 'fontsize=10');
+
+REPLACE INTO .multilingual_static_text
+(ml_page_id, page_item, language_id, text) VALUES
+-- Form 1 background check consent
+(51, 'form_id1_title', 1, 'Form Title Here'),
+(51, 'form_id1_wizard_instructions', 1, ''),
+(52, 'form_text', 1, '<div class="message download"><p>Please download and read, and complete the <a href="[PdfUrl]">Form Title Here (PDF)</a></p></div>');
